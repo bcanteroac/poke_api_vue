1dff086a8e0ecfc2cda08dea6c4d1895693e39c6
bcanteroac <91626533+bcanteroac@users.noreply.github.com>
2023-06-04 03:53:52 -0400

    main js config

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Bootstrap
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/js/bootstrap.js";
+
+//Instancia Aplicacion
+const app = createApp(App);
+
+//Importacion modulos
+app.use(router);
+
+//Montar Aplicacion
+app.mount('#app');
