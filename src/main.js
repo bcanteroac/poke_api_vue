9e26618639dad486f77ccd245d4037f642a6408c
bcanteroac <91626533+bcanteroac@users.noreply.github.com>
2023-06-04 05:55:20 -0400

    axios config

--- src/main.js
+++ src/main.js
@@ -6,11 +6,16 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
+// Axios
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 //Instancia Aplicacion
 const app = createApp(App);
 
 //Importacion modulos
 app.use(router);
+app.use(VueAxios, axios);
 
 //Montar Aplicacion
 app.mount('#app');
